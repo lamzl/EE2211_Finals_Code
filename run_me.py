73f0c4df299b7ac06f8a7c3072bacefb17e43bb1
ethanlamcode <137771880+ethanlamcode@users.noreply.github.com>
2026-04-30 23:33:42 +0800

    for wei xiong

--- run_me.py
+++ run_me.py
@@ -59,12 +59,9 @@ print("=" * 50)
 
 # X_train_raw = np.array([[1, 3, -2], [-4, 0, -1], [3, 1, 8], [2, 1, 6], [8, 4, 6]])
 X_train_raw = np.array([[4], [7], [10], [2], [3], [9]])
-
 y_train = np.array([-1, -1, -1, 1, 1, 1])
-
-
+X_test_raw = np.array([[4], [7], [10], [2], [3], [9]])
 # X_test_raw = np.array([[4], [7], [10], [2], [3], [9]])
-X_test_raw = np.array([[6]])
 
 # y_test = np.array([1, 1, 2, 3, 3])
 
